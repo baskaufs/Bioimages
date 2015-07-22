11b2602974a68fbb79faff2c5d4e2e5e1ee43dff
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2015-07-22 05:46:28 -0500

    deleted obsolete test files

--- check-screen-size.js
+++
@@ -1,9 +0,0 @@
-function checkScreenSize(hiresSAP,imgIRI,imgSource)
-     {
-document.getElementById("paste").setAttribute("class", browser); // set css for browser type
-notPortableDevice=((screen.availWidth > 500) || (screen.availHeight > 500));  //enable highres image for big screen
-if (notPortableDevice)
- {
- document.getElementById('replaceImage').innerHTML='<a href="'+hiresSAP+'"><img alt="'+imgIRI+'" src="'+imgSource+'" /></a>'; 
- }
-     }
