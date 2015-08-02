2bbe700b3dbbc5299b65e884df4d675769eb24a9
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2015-08-01 23:14:45 -0500

    fixed typo in xquery batch file

--- xquery/generate.bat
+++ xquery/generate.bat
@@ -1,8 +1,8 @@
 START /WAIT basex organism-html.xq
 START /WAIT basex organism-rdf.xq
-START /WAIT basex images.xq
+START /WAIT basex image.xq
 START /WAIT basex index-files.xq
 START /WAIT basex index-rss.xq
 START /WAIT basex index-sitemap.xq
-START /WAIT basex save-published-date.xq
+START /WAIT basex save-published-date.cdxq
 
